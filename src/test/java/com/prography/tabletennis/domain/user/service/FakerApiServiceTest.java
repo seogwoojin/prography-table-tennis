36feb618c32eb801c17ce313c68d4af3d1693af2
woojin <zaza0804@uos.ac.kr>
2025-02-15 22:15:00 +0900

    test: add Faker Api Test

---
+++ src/test/java/com/prography/tabletennis/domain/user/service/FakerApiServiceTest.java
@@ -0,0 +1,24 @@
+package com.prography.tabletennis.domain.user.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.prography.tabletennis.domain.init.service.FakerApiService;
+import com.prography.tabletennis.domain.init.dto.response.FakerApiResponse;
+
+@SpringBootTest
+class FakerApiServiceTest {
+	@Autowired
+	FakerApiService fakerApiService;
+
+	@Test
+	public void APITest() {
+		FakerApiResponse fakeUsers = fakerApiService.getFakeUsers(1, 1);
+
+		assertThat(Integer.parseInt(fakeUsers.getSeed())).isEqualTo(1);
+		assertThat(fakeUsers.getUserDataList().size()).isEqualTo(1);
+	}
+}
