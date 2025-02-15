5c2ff267eb8edcabd26a1da469f50faf9b8b9411
woojin <zaza0804@uos.ac.kr>
2025-02-15 22:13:50 +0900

    feat: Feign Client 도입 및 fakerApi 설정

---
+++ src/main/java/com/prography/tabletennis/domain/init/service/FakerApiService.java
@@ -0,0 +1,17 @@
+package com.prography.tabletennis.domain.init.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.prography.tabletennis.domain.init.dto.response.FakerApiResponse;
+
+@FeignClient(name = "FakerApi", url = "https://fakerapi.it")
+public interface FakerApiService {
+	@GetMapping("/api/v1/users?_locale=ko_KR")
+		// 일부 고정 파라미터만 기술
+	FakerApiResponse getFakeUsers(
+		@RequestParam(name = "_seed") int seed,
+		@RequestParam(name = "_quantity") int quantity
+	);
+}
