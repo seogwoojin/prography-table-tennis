8cd565c2e93eda7dfb04efdaa13683cd1c907512
woojin <zaza0804@uos.ac.kr>
2025-02-20 16:00:29 +0900

    refactor: TeamData Record 추가를 통한 중복 로직 제거

--- src/main/java/com/prography/tabletennis/domain/room/utils/TeamAssignmentService.java
+++ src/main/java/com/prography/tabletennis/domain/room/utils/TeamAssignmentService.java
@@ -13,13 +13,27 @@ import com.prography.tabletennis.domain.room.entity.enums.TeamType;
 import com.prography.tabletennis.global.response.CustomException;
 import com.prography.tabletennis.global.response.ReturnCode;
 
+/** 팀과 관련한 계산만을 담당하는 클래스 입니다. */
 @Component
 public class TeamAssignmentService {
+  /** 방의 Team 관련 정보를 래핑하는 Record 클래스입니다. extractTeamData(Room) 을 통해서 구할 수 있습니다. */
+  private record TeamData(Map<TeamType, Long> teamCounts, int maxPerTeam) {}
+
+  public TeamType assignTeam(Room room) {
+    TeamData teamData = extractTeamData(room);
+    return selectTeam(teamData.teamCounts(), teamData.maxPerTeam());
+  }
+
+  /** 팀 변경 가능 여부를 확인하는 함수 */
+  public boolean changeTeamPossible(Room room, TeamType presentTeam) {
+    TeamData teamData = extractTeamData(room);
+    TeamType wishTeam = presentTeam.changeTeam();
+    return teamData.teamCounts().get(wishTeam) < teamData.maxPerTeam();
+  }
+
   public boolean isEachTeamFull(Room room) {
-    List<UserRoom> userRooms = room.getUserRoomList();
-    Map<TeamType, Long> teamCounts = countTeamMembers(userRooms);
-    int maxPerTeam = calculateMaxPerTeam(room.getRoomType());
-    return compareMaxPerTeamWithCounts(teamCounts, maxPerTeam);
+    TeamData teamData = extractTeamData(room);
+    return compareMaxPerTeamWithCounts(teamData.teamCounts(), teamData.maxPerTeam);
   }
 
   private boolean compareMaxPerTeamWithCounts(Map<TeamType, Long> teamCounts, int maxPerTeam) {
@@ -28,29 +42,14 @@ public class TeamAssignmentService {
     return redCount == maxPerTeam && blueCount == maxPerTeam;
   }
 
-  public boolean changeTeamPossible(Room room, TeamType presentTeam) {
+  private TeamData extractTeamData(Room room) {
     List<UserRoom> userRooms = room.getUserRoomList();
     Map<TeamType, Long> teamCounts = countTeamMembers(userRooms);
     int maxPerTeam = calculateMaxPerTeam(room.getRoomType());
-    TeamType wishTeam = getWishTeam(presentTeam);
-    return teamCounts.get(wishTeam) < maxPerTeam;
+    return new TeamData(teamCounts, maxPerTeam);
   }
 
-  private TeamType getWishTeam(TeamType teamType) {
-    if (teamType == TeamType.RED) {
-      return TeamType.BLUE;
-    }
-    return TeamType.RED;
-  }
-
-  public TeamType assignTeam(Room room) {
-    List<UserRoom> userRooms = room.getUserRoomList();
-    Map<TeamType, Long> teamCounts = countTeamMembers(userRooms);
-    int maxPerTeam = calculateMaxPerTeam(room.getRoomType());
-    return selectTeam(teamCounts, maxPerTeam);
-  }
-
-  /** 팀별 인원 수를 계산합니다. */
+  /** 팀별 인원 수를 계산 */
   private Map<TeamType, Long> countTeamMembers(List<UserRoom> userRooms) {
     return userRooms.stream()
         .collect(Collectors.groupingBy(UserRoom::getTeamType, Collectors.counting()));
